d40a8cf0f3f9c013b1da0fca9729a2ecb6e9330d
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-05-08 22:23:13 +0300

    added some navigation setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from "react";
-
+import Header from "./components/header/header.component";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => (
+  <div>
+    <h1>TOPICS List</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL</h1>
+  </div>
+);
 
 function App() {
-  return <div><HomePage/></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route  component={HatsPage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route exact path="/topics/:topicId" component={TopicDetail} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
